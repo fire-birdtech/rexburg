445c6cdd7f98ace6ed7158046e33372ffe6a96f2
Jarom Rial <jarom.rial@gmail.com>
2021-10-11 13:05:25 -0600

    add slug configuration to Housing

--- app/Models/Housing.php
+++ app/Models/Housing.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Housing extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
     protected $fillable = [
         'about',
@@ -26,6 +28,26 @@ class Housing extends Model
         'tenant_rating',
         'website_url',
     ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     /**
      * Get all of the housing's reviews.
