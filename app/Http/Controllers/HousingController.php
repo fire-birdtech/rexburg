3c8abae84475e41d20b4064f2add7138f82462a1
Jarom Rial <jarom.rial@gmail.com>
2021-10-11 09:47:00 -0600

    move Housing fillable array from controller to model

--- app/Http/Controllers/HousingController.php
+++ app/Http/Controllers/HousingController.php
@@ -6,21 +6,5 @@ use Illuminate\Http\Request;
 
 class HousingController extends Controller
 {
-    protected $fillable = [
-        'about',
-        'amenities',
-        'bathroom_range',
-        'bedroom_range',
-        'city',
-        'email_address',
-        'housing_type',
-        'name',
-        'phone_number',
-        'postal_code',
-        'rent_range',
-        'slug',
-        'street',
-        'tenant_rating',
-        'website_url',
-    ];
+    //
 }
--- app/Models/Housing.php
+++ app/Models/Housing.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Housing extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'about',
+        'amenities',
+        'bathroom_range',
+        'bedroom_range',
+        'city',
+        'email_address',
+        'housing_type',
+        'name',
+        'phone_number',
+        'postal_code',
+        'rent_range',
+        'slug',
+        'street',
+        'tenant_rating',
+        'website_url',
+    ];
 }
