f5ab988e648a1035c59045adc1abe00f0a9dc822
Jarom Rial <jarom.rial@gmail.com>
2021-10-12 15:18:18 -0600

    setup admin dashboard with links

--- public/js/app.js
+++ public/js/app.js
@@ -40679,10 +40679,25 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/Layouts/AppLayout */ "./resources/js/Layouts/AppLayout.vue");
+/* harmony import */ var _inertiajs_inertia_vue3__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @inertiajs/inertia-vue3 */ "./node_modules/@inertiajs/inertia-vue3/dist/index.js");
+/* harmony import */ var _heroicons_vue_outline__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @heroicons/vue/outline */ "./node_modules/@heroicons/vue/outline/esm/index.js");
 
+
+
+var links = [{
+  name: "Housing",
+  route: route('admin.housing')
+}];
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   components: {
-    AppLayout: _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_0__.default
+    AppLayout: _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_0__.default,
+    ChevronRightIcon: _heroicons_vue_outline__WEBPACK_IMPORTED_MODULE_2__.ChevronRightIcon,
+    Link: _inertiajs_inertia_vue3__WEBPACK_IMPORTED_MODULE_1__.Link
+  },
+  setup: function setup() {
+    return {
+      links: links
+    };
   }
 });
 
@@ -44033,16 +44048,66 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
 var _hoisted_1 = {
-  "class": "py-16"
+  "class": "py-12 px-4 md:px-0"
+};
+var _hoisted_2 = {
+  "class": "max-w-7xl mx-auto space-y-8 sm:px-6 lg:px-8"
+};
+
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", null, "Admin Dashboard", -1
+/* HOISTED */
+);
+
+var _hoisted_4 = {
+  "class": "mt-4 bg-white shadow overflow-hidden sm:rounded-md"
+};
+var _hoisted_5 = {
+  role: "list",
+  "class": "divide-y divide-gray-200"
+};
+var _hoisted_6 = {
+  "class": "px-4 py-4 flex items-center sm:px-6"
+};
+var _hoisted_7 = {
+  "class": "min-w-0 flex-1 sm:flex sm:items-center sm:justify-between"
+};
+var _hoisted_8 = {
+  "class": "ml-5 flex-shrink-0"
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
+  var _component_ChevronRightIcon = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("ChevronRightIcon");
+
+  var _component_Link = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Link");
+
   var _component_app_layout = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("app-layout");
 
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(_component_app_layout, null, {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(_component_app_layout, {
+    title: "Admin Dashboard"
+  }, {
     "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_1, "Hello, " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(_ctx.$page.props.user.name), 1
-      /* TEXT */
-      )];
+      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [_hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("ul", _hoisted_5, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($setup.links, function (link, linkIdx) {
+        return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("li", {
+          key: linkIdx
+        }, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Link, {
+          href: link.route
+        }, {
+          "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+            return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_6, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(link.name), 1
+            /* TEXT */
+            )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_8, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_ChevronRightIcon, {
+              "class": "h-5 w-5 text-gray-400",
+              "aria-hidden": "true"
+            })])])];
+          }),
+          _: 2
+          /* DYNAMIC */
+
+        }, 1032
+        /* PROPS, DYNAMIC_SLOTS */
+        , ["href"])]);
+      }), 128
+      /* KEYED_FRAGMENT */
+      ))])])])])];
     }),
     _: 1
     /* STABLE */
--- resources/js/Pages/Admin/Home.vue
+++ resources/js/Pages/Admin/Home.vue
@@ -1,15 +1,48 @@
 <template>
-    <app-layout>
-        <div class="py-16">Hello, {{$page.props.user.name}}</div>
+    <app-layout title="Admin Dashboard">
+        <div class="py-12 px-4 md:px-0">
+            <div class="max-w-7xl mx-auto space-y-8 sm:px-6 lg:px-8">
+                <h2>Admin Dashboard</h2>
+                <div class="mt-4 bg-white shadow overflow-hidden sm:rounded-md">
+                    <ul role="list" class="divide-y divide-gray-200">
+                        <li v-for="(link, linkIdx) in links" :key="linkIdx">
+                            <Link :href="link.route">
+                                <div class="px-4 py-4 flex items-center sm:px-6">
+                                    <div class="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+                                        <span>{{ link.name }}</span>
+                                    </div>
+                                    <div class="ml-5 flex-shrink-0">
+                                        <ChevronRightIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                    </div>
+                                </div>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </app-layout>
 </template>
 
 <script>
     import AppLayout from '@/Layouts/AppLayout';
+    import { Link } from '@inertiajs/inertia-vue3';
+    import { ChevronRightIcon } from '@heroicons/vue/outline';
+
+    const links = [
+        { name: "Housing", route: route('admin.housing') },
+    ];
 
     export default {
         components: {
             AppLayout,
+            ChevronRightIcon,
+            Link,
+        },
+        setup() {
+            return {
+                links,
+            }
         },
     }
 </script>
