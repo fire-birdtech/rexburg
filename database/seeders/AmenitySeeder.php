090a0b7eada69dac937c448e6bd6b3be0c3a2955
Jarom Rial <jarom.rial@gmail.com>
2021-10-20 11:44:13 -0600

    seed amenities table

--- database/seeders/AmenitySeeder.php
+++ database/seeders/AmenitySeeder.php
@@ -13,6 +13,39 @@ class AmenitySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $amenities = [
+            'Air Conditioning',
+            'Washer & Dryer Hookups',
+            'Wheelchair Access',
+            'Laundry Facilities',
+            'Pool',
+            'Cat Friendly',
+            'Lofts',
+            'Patio',
+            'Hardwood Floors',
+            'Business Center',
+            'Playground',
+            'Walk-in Closets',
+            'In Unit Washer & Dryer',
+            'Dishwasher',
+            'Parking',
+            'Fitness Center',
+            'Elevator',
+            'Dog Friendly',
+            'Furnished',
+            'Utilities Included',
+            'Fireplace',
+            'Garage',
+            'Balcony',
+            'Clubhouse',
+            'Front Office',
+            'Theater Rooms',
+            'Close to Campus',
+            '24-hour Maintenance',
+            'On-site Maintenance',
+            'Barbecue Grill(s)',
+            'Online Payments',
+            'Recreation Room(s)'
+        ];
     }
 }
