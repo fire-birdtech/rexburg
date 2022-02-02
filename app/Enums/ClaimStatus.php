51c58765f186f309cb05fdb39e2dddf23135eb1a
Jarom Rial <jarom.rial@gmail.com>
2022-02-02 11:40:43 -0700

    add namespace to ClaimStatus

--- app/Enums/ClaimStatus.php
+++ app/Enums/ClaimStatus.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App;
+
 class ClaimStatus
 {
     const PENDING = 'pending';
--- app/Http/Controllers/ClaimController.php
+++ app/Http/Controllers/ClaimController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Claim;
 use App\Models\Housing;
-use ClaimStatus;
+use App\ClaimStatus;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
