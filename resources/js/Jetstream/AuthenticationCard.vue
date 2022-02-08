0c288e1f23c128aeae4a15346ebccb57e14c1137
Jarom Rial <jarom.rial@gmail.com>
2022-02-08 11:14:53 -0700

    add x padding to logo on auth pages

--- resources/js/Jetstream/AuthenticationCard.vue
+++ resources/js/Jetstream/AuthenticationCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
+        <div class="px-4 sm:px-0">
             <slot name="logo" />
         </div>
 
