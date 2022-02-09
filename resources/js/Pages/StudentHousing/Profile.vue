77a056456f506333a2760d59395b85c7ea66b714
Jarom Rial <jarom.rial@gmail.com>
2022-02-08 19:46:39 -0700

    remove type attribute from edit profile link

--- resources/js/Pages/StudentHousing/Profile.vue
+++ resources/js/Pages/StudentHousing/Profile.vue
@@ -20,7 +20,7 @@
                 </div>
                 <div v-if="$page.props.user" class="ml-auto space-x-4">
                     <button @click="createClaim" v-if="$page.props.user && ! isClaimed" class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:mt-0">Claim</button>
-                    <Link :href="route('housing.profile.edit', [housing.slug])" v-if="(housing.manager?.user_id === $page.props.user.id) || isAdmin" type="button" class="mt-3 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:mt-0">
+                    <Link :href="route('housing.profile.edit', [housing.slug])" v-if="(housing.manager?.user_id === $page.props.user.id) || isAdmin" class="mt-3 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:mt-0">
                         Edit profile
                     </Link>
                 </div>
