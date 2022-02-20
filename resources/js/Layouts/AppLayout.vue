e0981cd3e03323f4f81d16c7aaa7474e56d4dbc3
Jarom Rial <jarom.rial@gmail.com>
2022-02-19 19:26:50 -0700

    update user profile button and dropdown in the main navigation

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -48,59 +48,39 @@
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
                             <!-- User Dropdown -->
-                            <div class="ml-3 relative">
-                                <template v-if="$page.props.user">
-                                    <jet-dropdown align="right" width="48">
-                                        <template #trigger>
-                                            <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                                <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name" />
-                                            </button>
-
-                                            <span v-else class="inline-flex rounded-md">
-                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-sky-500 hover:bg-sky-600 focus:outline-none transition">
-                                                    {{ $page.props.user?.name }}
-
-                                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                                    </svg>
-                                                </button>
-                                            </span>
-                                        </template>
-
-                                        <template #content>
-                                            <!-- Account Management -->
-                                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                                Manage Account
-                                            </div>
-
-                                            <jet-dropdown-link :href="route('profile.show')">
-                                                Profile
-                                            </jet-dropdown-link>
-
-                                            <div class="border-t border-gray-100"></div>
-
-                                            <!-- Authentication -->
-                                            <form @submit.prevent="logout">
-                                                <jet-dropdown-link as="button">
-                                                    Log Out
-                                                </jet-dropdown-link>
-                                            </form>
-                                        </template>
-                                    </jet-dropdown>
-                                </template>
-
-                                <template v-else>
-                                    <div class="ml-10 space-x-4">
-                                        <Link :href="route('login')" class="inline-block bg-sky-400 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75">
-                                            Log in
-                                        </Link>
-
-                                        <Link v-if="canRegister" :href="route('register')" class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-sky-600 hover:bg-sky-100">
-                                            Register
-                                        </Link>
+                            <template v-if="$page.props.user">
+                                <Menu as="div" class="ml-3 relative">
+                                    <div>
+                                        <MenuButton class="max-w-xs bg-sky-500 rounded-full flex items-center text-sm focus:outline-none lg:p-2 lg:rounded-md lg:hover:bg-sky-600 lg:hover:bg-opacity-75">
+                                            <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name" />
+                                            <span class="hidden ml-3 text-white text-sm font-medium lg:block"><span class="sr-only">Open user menu for </span>{{ $page.props.user.name }}</span>
+                                            <ChevronDownIcon class="hidden flex-shrink-0 ml-1 h-5 w-5 text-sky-50 lg:block" aria-hidden="true" />
+                                        </MenuButton>
                                     </div>
-                                </template>
-                            </div>
+                                    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                                        <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <MenuItem v-slot="{ active }">
+                                                <Link :href="route('profile.show')" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</Link>
+                                            </MenuItem>
+                                            <MenuItem v-slot="{ active }">
+                                                <Link :href="route('logout')" method="POST" as="button" :class="[active ? 'bg-gray-100' : '', 'block w-full text-left px-4 py-2 text-sm text-gray-700']">Logout</Link>
+                                            </MenuItem>
+                                        </MenuItems>
+                                    </transition>
+                                </Menu>
+                            </template>
+
+                            <template v-else>
+                                <div class="ml-10 space-x-4">
+                                    <Link :href="route('login')" class="inline-block bg-sky-400 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75">
+                                        Log in
+                                    </Link>
+
+                                    <Link v-if="canRegister" :href="route('register')" class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-sky-600 hover:bg-sky-100">
+                                        Register
+                                    </Link>
+                                </div>
+                            </template>
                         </div>
 
                         <!-- Hamburger -->
@@ -187,6 +167,7 @@
     import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
     import { ChevronDownIcon, UserIcon, UsersIcon } from '@heroicons/vue/solid';
     import Notification from '@/Components/Notification';
+    import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
 
     const housing = [
         { name: "Single Housing", href: route('housing.single'), icon: UserIcon },
@@ -210,6 +191,10 @@
             JetNavLink,
             JetResponsiveNavLink,
             Link,
+            Menu,
+            MenuButton,
+            MenuItem,
+            MenuItems,
             Notification,
             Popover,
             PopoverButton,
