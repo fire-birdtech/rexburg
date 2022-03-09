79d1480dc212be334241fa587aaba676dd221c65
Jarom Rial <jarom.rial@gmail.com>
2022-03-08 18:04:31 -0700

    update claim workflow on housing profile
    - change claim button to be under housing name
    - add modal that is trigger by claim button

--- resources/js/Pages/StudentHousing/Profile.vue
+++ resources/js/Pages/StudentHousing/Profile.vue
@@ -19,7 +19,6 @@
                     </div>
                 </div>
                 <div v-if="$page.props.user" class="ml-auto space-x-4">
-                    <button @click="createClaim" v-if="$page.props.user && ! isClaimed" class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:mt-0">Claim</button>
                     <Link :href="route('housing.profile.edit', [housing.slug])" v-if="housing.manager?.user_id === $page.props.user.id" class="mt-3 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:mt-0">
                         Edit profile
                     </Link>
@@ -69,7 +68,14 @@
                 </aside>
                 <div class="mt-8 lg:w-2/3 md:mt-2 md:order-1">
                     <div class="md:px-4 md:py-5">
-                        <p class="text-xl font-medium text-gray-700">{{ `${housing.street}, ${housing.city}, Idaho ${housing.postal_code}` }}</p>
+                        <button v-if="$page.props.user && ! isClaimed" @click="claimDialogOpen = true" class="inline-flex space-x-4 group">
+                            <span class="rounded bg-sky-200 px-2.5 py-1 text-xs font-semibold text-sky-700 tracking-wide uppercase"> Claim this housing </span>
+                            <span class="inline-flex items-center text-sm font-medium text-sky-700 space-x-1 group-hover:text-sky-900">
+                                <span>Do you manage {{ housing.name }}?</span>
+                                <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
+                            </span>
+                        </button>
+                        <p class="mt-6 text-xl font-medium text-gray-700">{{ `${housing.street}, ${housing.city}, Idaho ${housing.postal_code}` }}</p>
                         <div class="flex flex-col space-y-3 pt-4 text-gray-600 sm:space-x-6 sm:space-y-0 sm:mt-0 sm:flex-row sm:flex-wrap">
                             <template v-if="housing.housing_type == 'single'">
                                 <div v-if="housing.byui_approved" class="flex items-center text-md leading-5">
@@ -154,6 +160,38 @@
                 </div>
             </div>
         </div>
+        
+        <TransitionRoot as="template" :show="claimDialogOpen">
+            <Dialog as="div" class="fixed z-10 inset-0 overflow-y-auto" @close="open = false">
+                <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                    <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                        <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    </TransitionChild>
+
+                    <!-- This element is to trick the browser into centering the modal contents. -->
+                    <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                    <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                        <div class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                            <div>
+                                <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-sky-100">
+                                    <BadgeCheckIcon class="h-6 w-6 text-sky-600" aria-hidden="true" />
+                                </div>
+                                <div class="mt-3 text-center sm:mt-5">
+                                    <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900"> Claim {{ housing.name }} </DialogTitle>
+                                    <div class="mt-3">
+                                        <p class="text-base text-gray-600">By clicking "Claim" below, you verify that you are either the owner or manager of {{ housing.name }} and accept the responsibilty of maintaining this profile.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+                                <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-sky-600 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:col-start-2 sm:text-sm" @click="open = false">Claim</button>
+                                <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:mt-0 sm:col-start-1 sm:text-sm" @click="claimDialogOpen = false" ref="cancelButtonRef">Cancel</button>
+                            </div>
+                        </div>
+                    </TransitionChild>
+                </div>
+            </Dialog>
+        </TransitionRoot>
     </app-layout>
 </template>
 
@@ -161,12 +199,13 @@
     import { defineComponent, ref } from 'vue';
     import { Link } from '@inertiajs/inertia-vue3';
     import AppLayout from '@/Layouts/AppLayout.vue';
-    import { BadgeCheckIcon, CheckCircleIcon, CheckIcon, HomeIcon, LocationMarkerIcon, MailIcon, PhoneIcon, StarIcon, UserIcon, UsersIcon } from '@heroicons/vue/solid';
+    import { BadgeCheckIcon, CheckCircleIcon, CheckIcon, ChevronRightIcon, HomeIcon, LocationMarkerIcon, MailIcon, PhoneIcon, StarIcon, UserIcon, UsersIcon } from '@heroicons/vue/solid';
     import { DeviceMobileIcon, MailIcon as MailOutlineIcon } from '@heroicons/vue/outline';
     import TextEditor from '@/Components/TextEditor';
     import Stars from '@/Components/Stars';
     import JetInputError from '@/Jetstream/InputError';
     import ReviewList from '@/Components/ReviewList';
+    import { Dialog, DialogOverlay, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue';
 
     export default defineComponent({
         components: {
@@ -174,7 +213,11 @@
             BadgeCheckIcon,
             CheckCircleIcon,
             CheckIcon,
+            ChevronRightIcon,
             DeviceMobileIcon,
+            Dialog,
+            DialogOverlay,
+            DialogTitle,
             HomeIcon,
             JetInputError,
             Link,
@@ -186,6 +229,8 @@
             StarIcon,
             Stars,
             TextEditor,
+            TransitionChild,
+            TransitionRoot,
             UserIcon,
             UsersIcon,
         },
@@ -223,9 +268,19 @@
             },
             createClaim() {
                 this.form.post(route('claims.create'), {
-                    onSuccess: () => (this.isClaimed = true),
+                    onSuccess: () => {
+                        this.isClaimed = true;
+                        this.claimDialogOpen = false;
+                    },
                 });
             },
+        },
+        setup() {
+            const claimDialogOpen = ref(false);
+            
+            return {
+                claimDialogOpen,
+            }
         },
     })
 </script>
