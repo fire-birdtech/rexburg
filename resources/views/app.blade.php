b2d21a38ffe99aa34818d08581328cfebc7dee39
Jarom Rial <jarom.rial@gmail.com>
2022-05-27 05:57:05 -0600

    add analytics code to site head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,24 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        @if(env('APP_ENV') === 'production')
+        <!-- Matomo -->
+        <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//analytics.jarom.dev/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '3']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+        </script>
+        <!-- End Matomo Code -->
+        @endif
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
