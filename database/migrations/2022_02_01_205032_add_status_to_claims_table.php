53089dd6bf510fab27aa6809f380482ec965151f
Shift <shift@laravelshift.com>
2023-03-29 12:27:52 +0000

    Adopt anonymous migrations

--- database/migrations/2022_02_01_205032_add_status_to_claims_table.php
+++ database/migrations/2022_02_01_205032_add_status_to_claims_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddStatusToClaimsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class AddStatusToClaimsTable extends Migration
             $table->dropColumn('status');
         });
     }
-}
+};
