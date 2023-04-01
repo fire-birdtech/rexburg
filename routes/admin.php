57d38a3166521a32b56ce703a5dec834c7f30ef2
Jarom Rial <jarom.rial@gmail.com>
2023-04-01 06:02:51 -0600

    create admin routes

---
+++ routes/admin.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Admin\ClaimsController as AdminClaimsController;
+use App\Http\Controllers\Admin\HousingController as AdminHousingController;
+use App\Http\Controllers\Admin\PagesController as AdminPagesController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum', 'verified', 'admin'])->prefix('admin')->group(function() {
+    Route::name('admin.')->group(function() {
+        Route::get('/', [AdminPagesController::class, 'home'])->name('home');
+        Route::prefix('housing')->name('housing.')->group(function() {
+            Route::get('/', [AdminHousingController::class, 'index'])->name('index');
+            Route::get('create', [AdminHousingController::class, 'create'])->name('create');
+            Route::post('store', [AdminHousingController::class, 'store'])->name('store');
+            Route::get('{id}', [AdminHousingController::class, 'show'])->name('show');
+            Route::post('{id}', [AdminHousingController::class, 'update'])->name('update');
+            Route::get('{id}/edit', [AdminHousingController::class, 'edit'])->name('edit');
+        });
+        Route::prefix('users')->name('users.')->group(function () {
+            Route::get('/', [AdminUserController::class, 'index'])->name('index');
+            Route::get('{id}', [AdminUserController::class, 'show'])->name('show');
+            Route::post('/{id}/suspend', [AdminUserController::class, 'suspend'])->name('suspend');
+            Route::delete('{user}', [AdminUserController::class, 'destroy'])->name('delete');
+        });
+        Route::prefix('claims')->name('claims.')->group(function () {
+            Route::get('/', [AdminClaimsController::class, 'index'])->name('index');
+            Route::get('{id}', [AdminClaimsController::class, 'show'])->name('show');
+            Route::post('{id}', [AdminClaimsController::class, 'approve'])->name('approve');
+            Route::delete('{id}', [AdminClaimsController::class, 'reject'])->name('reject');
+        });
+    });
+});
