e24366d01c3a70cf888347a52fc7c5dc40e59186
Jarom Rial <jarom.rial@gmail.com>
2023-04-01 05:06:54 -0600

    update housing card

--- resources/js/Components/HousingCard.vue
+++ resources/js/Components/HousingCard.vue
@@ -7,36 +7,36 @@ defineProps(['housing']);
 </script>
 
 <template>
-    <article class="scale-100 relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-slate-900 px-8 pb-5 pt-60 motion-safe:hover:scale-[1.01] sm:pt-48 lg:pt-60">
-        <img :src="housing.cover_image_url" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover" />
-        <div class="absolute inset-0 -z-10 bg-gradient-to-t from-slate-800 via-slate-900/40" />
-        <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-slate-900/10" />
-
-        <div class="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-slate-300">
-            <div class="flex items-center">
-                <Stars size="h-4 w-4" :score="housing.score" />
-                <span class="ml-2 font-semibold">{{ housing.score }}</span>
-                <span class="ml-4">{{ housing.reviews_count }} {{ housing.reviews_count === 1 ? 'review' : 'reviews' }}</span>
-            </div>
-        </div>
-        <div class="mt-2 flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-slate-300">
-            <div class="flex items-center gap-x-4">
-                <svg viewBox="0 0 2 2" class="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
-                    <circle cx="1" cy="1" r="1" />
-                </svg>
-                <div class="flex gap-x-2.5 text-slate-100">
-                    <!-- <img :src="post.author.imageUrl" alt="" class="h-6 w-6 flex-none rounded-full bg-white/10" /> -->
-                    {{ housing.rent_range }}
-                    <span v-if="housing.housing_type === 'single'" class="ml-1 text-sm text-slate-300">/semester</span>
-                    <span v-if="housing.housing_type === 'married'" class="ml-1 text-sm text-slate-300">/month</span>
+    <Link
+        :href="route('housing.profile', [housing.slug])"
+        class="scale-100 p-6 bg-white dark:bg-slate-800/50 dark:bg-gradient-to-bl from-slate-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-slate-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-sky-500"
+    >
+        <img class="absolute inset-0 h-full w-full rounded-lg object-cover brightness-50 saturate-40" :src="housing.cover_image_url" :alt="housing.name" />
+        <div class="relative flex">
+            <div class="pt-32">
+                <div class="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-slate-300">
+                    <div class="flex items-center">
+                        <Stars size="h-4 w-4" :score="housing.score" />
+                        <span class="ml-2 font-semibold">{{ housing.score }}</span>
+                        <span class="ml-4">{{ housing.reviews_count }} {{ housing.reviews_count === 1 ? 'review' : 'reviews' }}</span>
+                    </div>
                 </div>
+                <div class="mt-2 flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-slate-300">
+                    <div class="flex items-center gap-x-4">
+                        <div class="flex items-baseline text-slate-100">
+                            {{ housing.rent_range }}
+                            <span v-if="housing.housing_type === 'single'" class="ml-1 text-sm text-slate-300">/semester</span>
+                            <span v-if="housing.housing_type === 'married'" class="ml-1 text-sm text-slate-300">/month</span>
+                        </div>
+                    </div>
+                </div>
+                <h3 class="mt-3 text-lg font-semibold leading-6 text-white">
+                    <Link :href="route('housing.profile', [housing.slug])">
+                        <span class="absolute inset-0" />
+                        {{ housing.name }}
+                    </Link>
+                </h3>
             </div>
         </div>
-        <h3 class="mt-3 text-lg font-semibold leading-6 text-white">
-            <Link href="#">
-                <span class="absolute inset-0" />
-                {{ housing.name }}
-            </Link>
-        </h3>
-    </article>
+    </Link>
 </template>
