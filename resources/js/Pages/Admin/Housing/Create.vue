d9c87470b4f3eb55b00259f21238a02649aa9f7f
Jarom Rial <jarom.rial@gmail.com>
2023-08-09 20:55:51 -0600

    update icon import

--- resources/js/Pages/Admin/Housing/Create.vue
+++ resources/js/Pages/Admin/Housing/Create.vue
@@ -24,7 +24,7 @@
                                     <ListboxButton class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                         <span class="block truncate">{{ selected.name }}</span>
                                         <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                            <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                            <ArrowsUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
                                         </span>
                                     </ListboxButton>
 
@@ -64,7 +64,7 @@
     import { ref } from 'vue';
     import AdminLayout from '@/Layouts/AdminLayout.vue';
     import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue';
-    import { CheckIcon, SelectorIcon } from '@heroicons/vue/24/solid';
+    import { CheckIcon, ArrowsUpDownIcon } from '@heroicons/vue/24/solid';
     import JetInputError from '@/Jetstream/InputError.vue';
 
     const types = [
@@ -82,7 +82,7 @@
             ListboxLabel,
             ListboxOption,
             ListboxOptions,
-            SelectorIcon,
+            ArrowsUpDownIcon,
         },
         data() {
             return {
