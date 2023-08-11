2e65e9395ff46162bf58cf7993f073541e9b0598
Jarom Rial <jarom.rial@gmail.com>
2023-08-11 16:01:16 -0600

    update js configuration files

---
+++ resources/js/app.tsx
@@ -0,0 +1,38 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h, DefineComponent } from 'vue';
+import { createInertiaApp as createInertiaAppVue } from '@inertiajs/vue3';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaAppVue({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
