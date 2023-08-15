241c72a01b8bb10f74d0c6782177e044b4e2b803
Jarom Rial <jarom.rial@gmail.com>
2023-08-14 19:54:18 -0600

    add flyout menu for housing and update user dropdown to match

--- resources/js/Components/MainNav.tsx
+++ resources/js/Components/MainNav.tsx
@@ -1,8 +1,8 @@
 import { Fragment, useState } from 'react';
 import { Link } from '@inertiajs/react';
-import { Menu, Transition } from '@headlessui/react';
+import { Menu, Popover, Transition } from '@headlessui/react';
 import { Bars3Icon } from '@heroicons/react/24/outline';
-import { ChevronDownIcon } from '@heroicons/react/24/solid';
+import { ChevronDownIcon, UserIcon, UsersIcon } from '@heroicons/react/24/solid';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import classNames from '@/Utils/classNames';
 import { User } from '@/types';
@@ -12,7 +12,23 @@ type NavProps = {
 }
 
 const navigation = [
-  { name: 'Housing', href: '#' },
+  {
+    name: 'Housing',
+    links: [
+      {
+        name: 'Single Housing',
+        href: route('housing.single'),
+        icon: UserIcon,
+        description: 'Find BYU-I approved housing for single students. You can get private rooms or apartments ranging from 4-8 people.',
+      },
+      {
+        name: 'Married Housing',
+        href: route('housing.married'),
+        icon: UsersIcon,
+        description: 'Married housing is crucial, but difficult to find. We\'re making it easier. You can find different sizes and amenities.',
+      },
+    ],
+  },
   //   { name: 'Businesses', href: '#' },
   //   { name: 'Jobs', href: '#' },
   //   { name: 'Rides', href: '#' },
@@ -39,43 +55,77 @@ export default function MainNav({ user }: NavProps) {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <Link href={item.href} key={item.name} className="text-sm font-semibold leading-6 text-slate-400 hover:text-white">
-              {item.name}
-            </Link>
+            // <Link href={item.href} key={item.name} className="text-sm font-semibold leading-6 text-slate-400 hover:text-white">
+            //   {item.name}
+            // </Link>
+            <Popover key={item.name} className="relative">
+              <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-slate-400 hover:text-white focus:outline-none">
+                <span>{item.name}</span>
+                <ChevronDownIcon className="h-3 w-3" aria-hidden="true" />
+              </Popover.Button>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel className="absolute left-1/2 z-10 mt-4 flex w-screen max-w-max -translate-x-1/2 px-4">
+                  <div className="w-screen max-w-md flex-auto overflow-hidden rounded-2xl bg-slate-800 text-sm leading-6 shadow-lg ring-1 ring-slate-900/5">
+                    <div className="p-4">
+                      {item.links.map((link, index) => (
+                        <div key={index} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-slate-700">
+                          <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-slate-600 group-hover:bg-slate-500">
+                            <link.icon className="h-6 w-6 text-slate-300 group-hover:text-slate-200" aria-hidden="true" />
+                          </div>
+                          <div>
+                            <Link href={link.href} className="font-semibold text-slate-100">
+                              {link.name}
+                              <span className="absolute inset-0" />
+                            </Link>
+                            <p className="mt-1 text-slate-400">{link.description}</p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
           {user ? (
-            // <Link href={route('dashboard')} className="text-sm font-semibold leading-6 text-slate-400 hover:text-white">
-            //   Dashboard <span aria-hidden="true">&rarr;</span>
-            // </Link>
             <Menu as="div" className="relative inline-block text-left">
               <div>
-                <Menu.Button className="inline-flex items-center text-sm font-semibold leading-6 text-slate-400 hover:text-white">
+                <Menu.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-slate-400 hover:text-white">
                   <span className="sr-only">Open menu</span>
                   {user.name}
-                  <ChevronDownIcon className="ml-2 -mr-0.5 h-4 w-4" />
+                  <ChevronDownIcon className="-mr-0.5 h-3 w-3" />
                 </Menu.Button>
               </div>
 
               <Transition
                 as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
               >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-slate-800 shadow-lg ring-1 ring-black/5 focus:outline-none">
-                  <div className="py-1">
+                <Menu.Items className="absolute right-0 z-10 mt-4 w-48 origin-top-right rounded-2xl bg-slate-800 shadow-lg ring-1 ring-slate-900/5 focus:outline-none">
+                  <div className="p-4 font-semibold">
                     <Menu.Item>
                       {({ active }) => (
                         <Link
                           href={route('profile.edit')}
                           className={classNames(
-                            active ? 'bg-slate-700 text-white' : 'text-slate-300',
-                            'block px-4 py-2 text-sm',
+                            active ? 'text-white' : 'text-slate-300',
+                            'block p-2 text-sm',
                           )}
                         >
                           Profile
@@ -89,8 +139,8 @@ export default function MainNav({ user }: NavProps) {
                           method="post"
                           as="button"
                           className={classNames(
-                            active ? 'bg-slate-700 text-white' : 'text-slate-300',
-                            'block w-full text-left px-4 py-2 text-sm',
+                            active ? 'text-white' : 'text-slate-300',
+                            'block w-full text-left p-2 text-sm',
                           )}
                         >
                           Log Out
