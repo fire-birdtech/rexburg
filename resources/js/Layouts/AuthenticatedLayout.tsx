98545382cd7eaf37b44c6cb360e3c93175b9a4f6
Jarom Rial <jarom.rial@gmail.com>
2023-08-16 07:03:27 -0600

    adjust authenticated layout to display a minimum height of the screen

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -4,7 +4,7 @@ import { User } from '@/types';
 
 export default function Authenticated({ user, children }: PropsWithChildren<{ user: User }>) {
   return (
-    <div className="bg-slate-900 bg-dots-lighter selection:bg-sky-500 selection:text-white">
+    <div className="min-h-screen bg-slate-900 bg-dots-lighter selection:bg-sky-500 selection:text-white">
       <MainNav user={user} />
 
       <main className="py-20">
