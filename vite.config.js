faf135424b29c307428e2346c99e7e562bb3e234
Jarom Rial <jarom.rial@gmail.com>
2023-08-24 10:03:44 -0600

    remove vue from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -8,14 +7,6 @@ export default defineConfig({
     laravel({
       input: 'resources/js/app.tsx',
       refresh: true,
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
-        },
-      },
     }),
     react(),
   ],
