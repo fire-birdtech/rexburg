c7bc673ec6e3bb99501a2be36f99a9da89017791
Jarom Rial <jarom.rial@gmail.com>
2023-08-26 05:39:24 -0600

    move profile views to card component

--- resources/js/Pages/Manager/Dashboard.tsx
+++ resources/js/Pages/Manager/Dashboard.tsx
@@ -1,8 +1,7 @@
 import { Head } from '@inertiajs/react';
-import {
-  AreaChart, Card, Flex, Grid, Text,
-} from '@tremor/react';
+import { Grid } from '@tremor/react';
 import Container from '@/Components/Container';
+import ProfileViewsCard from '@/Components/Cards/ProfileViewsCard';
 import Authenticated from '@/Layouts/AuthenticatedLayout';
 import { Housing, PageProps } from '@/types';
 
@@ -17,23 +16,7 @@ export default function ManagerDashboard({ auth, housings, views }: PageProps & 
       <Container>
         <h2 className="text-xl font-semibold text-slate-100">Hello, {auth.user.name.split(' ')[0]}!</h2>
         <Grid numItems={2} className="mt-8">
-          <Card>
-            <Flex alignItems="start">
-              <Text>Profile Views</Text>
-            </Flex>
-            <AreaChart
-              className="mt-6 h-32"
-              data={views}
-              categories={housingNames}
-              colors={['sky', 'blue']}
-              index="Day"
-              showXAxis={true}
-              showGridLines={false}
-              startEndOnly={true}
-              showYAxis={false}
-              showLegend={false}
-            />
-          </Card>
+          <ProfileViewsCard names={housingNames} views={views}/>
         </Grid>
       </Container>
     </Authenticated>
