c1ae6f88482c994bc5455f0c6419814625feb993
Jarom Rial <jarom.rial@gmail.com>
2023-08-30 14:18:00 -0600

    (pint) update php code styling

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -12,35 +12,35 @@ use Inertia\ResponseFactory;
 
 class DashboardController extends Controller
 {
-  public function main(): Response|ResponseFactory
-  {
-    $range = CarbonPeriod::create(now()->subDays(30), now()->subDay());
-    $views = [];
-    foreach ($range as $date) {
-      $views[] = [
-        'Day' => $date->format('M d'),
-      ];
-    }
-    $profileViews = View::select([
-      DB::raw('count(*) as count'),
-      DB::raw('DATE(created_at) as day'),
-    ])
-    ->where([
-      ['created_at', '!=', Carbon::now()],
-      ['created_at', '>', Carbon::now()->subDays(30)],
-    ])
-    ->groupBy('day')
-    ->get();
-    foreach ($range as $key => $date) {
-      if ($profileViews->doesntContain('day', $date->format('Y-m-d'))) {
-        $views[$key]['Views'] = 0;
-      } else {
-        $views[$key]['Views'] = $profileViews->where('day', $date->format('Y-m-d'))->first()->getAttribute('count');
-      }
-    }
+    public function main(): Response|ResponseFactory
+    {
+        $range = CarbonPeriod::create(now()->subDays(30), now()->subDay());
+        $views = [];
+        foreach ($range as $date) {
+            $views[] = [
+                'Day' => $date->format('M d'),
+            ];
+        }
+        $profileViews = View::select([
+            DB::raw('count(*) as count'),
+            DB::raw('DATE(created_at) as day'),
+        ])
+            ->where([
+                ['created_at', '!=', Carbon::now()],
+                ['created_at', '>', Carbon::now()->subDays(30)],
+            ])
+            ->groupBy('day')
+            ->get();
+        foreach ($range as $key => $date) {
+            if ($profileViews->doesntContain('day', $date->format('Y-m-d'))) {
+                $views[$key]['Views'] = 0;
+            } else {
+                $views[$key]['Views'] = $profileViews->where('day', $date->format('Y-m-d'))->first()->getAttribute('count');
+            }
+        }
 
-    return inertia('Admin/Dashboards/Main', [
-      'views' => $views,
-    ]);
-  }
+        return inertia('Admin/Dashboards/Main', [
+            'views' => $views,
+        ]);
+    }
 }
