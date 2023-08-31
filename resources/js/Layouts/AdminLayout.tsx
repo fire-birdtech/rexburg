1c9aaa0d29540aa116f3ea8991db613f41ba8a31
Jarom Rial <jarom.rial@gmail.com>
2023-08-31 06:38:51 -0600

    adjust vertical spacing around children of admin layout

--- resources/js/Layouts/AdminLayout.tsx
+++ resources/js/Layouts/AdminLayout.tsx
@@ -8,7 +8,7 @@ export default function Admin({ user, children }: PropsWithChildren<{ user: User
       <AdminNav user={user} />
 
       <div className="lg:pl-72">
-        <main className="py-10">
+        <main className="py-4 lg:py-10">
           <div className="px-4 sm:px-6 lg:px-8">
             {children}
           </div>
