8b4922837c828adb51a2e60fc3c95455825a5efe
Jarom Rial <jarom.rial@gmail.com>
2023-08-30 20:50:23 -0600

    display user profile photo in admin navigation

--- resources/js/Components/AdminNav.tsx
+++ resources/js/Components/AdminNav.tsx
@@ -173,8 +173,8 @@ export default function AdminNav({ user }: NavProps) {
                     <div className="flex flex-1 items-center gap-x-3">
                       <img
                         className="h-6 w-6 shrink-0 rounded-full bg-slate-900"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        src={user.profile_photo_url}
+                        alt={user.name}
                       />
                       <span aria-hidden="true">
                         {user.name}
