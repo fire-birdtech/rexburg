1a9f3ed94e3f46176b23759c355349dc41c3f3b9
Jarom Rial <jarom.rial@gmail.com>
2023-08-31 06:37:38 -0600

    adjust horizontal spacing on admin main dashboard

--- resources/js/Pages/Admin/Dashboards/Main.tsx
+++ resources/js/Pages/Admin/Dashboards/Main.tsx
@@ -18,46 +18,44 @@ export default function MainDashboard({ auth, views }: PageProps & { views: neve
     >
       <Head title="Admin Dashboard" />
 
-      <div className="px-4 md:px-0">
-        <div className="max-w-8xl mx-auto space-y-8 sm:px-6 lg:px-8">
-          <h2 className="text-slate-200">
-            Admin Dashboard
-          </h2>
-          <Card className="w-full">
-            <Flex alignItems="start">
-              <Text>
-                Profile Views
-              </Text>
-            </Flex>
-            <AreaChart
-              className="mt-4 h-32"
-              data={views}
-              categories={['Views']}
-              index="Day"
-              colors={['sky']}
-              showXAxis={true}
-              showGridLines={true}
-              startEndOnly={true}
-              showYAxis={false}
-              showLegend={false}
-            />
-          </Card>
-          <div className="mt-4 overflow-hidden bg-white shadow sm:rounded-md">
-            <ul className="divide-y divide-gray-200" role="list">
-              {links.map((link, index) => (
-                <Link key={index} href={link.route}>
-                  <div className="flex items-center p-4 sm:px-6">
-                    <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
-                      {link.name}
-                    </div>
-                    <div className="ml-5 shrink-0">
-                      <ChevronRightIcon className="h-5 w-5 text-gray-400" />
-                    </div>
+      <div className="max-w-8xl mx-auto space-y-8 sm:px-6 lg:px-8">
+        <h2 className="text-slate-200">
+          Admin Dashboard
+        </h2>
+        <Card className="w-full">
+          <Flex alignItems="start">
+            <Text>
+              Profile Views
+            </Text>
+          </Flex>
+          <AreaChart
+            className="mt-4 h-32"
+            data={views}
+            categories={['Views']}
+            index="Day"
+            colors={['sky']}
+            showXAxis={true}
+            showGridLines={true}
+            startEndOnly={true}
+            showYAxis={false}
+            showLegend={false}
+          />
+        </Card>
+        <div className="mt-4 overflow-hidden bg-white shadow sm:rounded-md">
+          <ul className="divide-y divide-gray-200" role="list">
+            {links.map((link, index) => (
+              <Link key={index} href={link.route}>
+                <div className="flex items-center p-4 sm:px-6">
+                  <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+                    {link.name}
                   </div>
-                </Link>
-              ))}
-            </ul>
-          </div>
+                  <div className="ml-5 shrink-0">
+                    <ChevronRightIcon className="h-5 w-5 text-gray-400" />
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </ul>
         </div>
       </div>
     </Admin>
