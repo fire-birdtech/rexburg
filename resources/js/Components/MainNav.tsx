d4037dacaee006bc86297098e3949cdb05b45669
Jarom Rial <jarom.rial@gmail.com>
2023-08-31 09:34:06 -0600

    update main navigation mobile menu

--- resources/js/Components/MainNav.tsx
+++ resources/js/Components/MainNav.tsx
@@ -36,16 +36,24 @@ const navigation = [
 ];
 
 const mobileNavigation = [
-  { name: 'Single Housing', href: route('housing.single') },
-  { name: 'Married Housing', href: route('housing.married') },
+  {
+    name: 'Single Housing',
+    href: route('housing.single')
+  },
+  {
+    name: 'Married Housing',
+    href: route('housing.married')
+  },
 ];
 
 export default function MainNav({ user }: NavProps) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const dashboardLink = () => {
-    const isManager = user?.roles.map((role: { name: string; }) => role.name).indexOf('manager');
-    const isAdmin = user?.roles.map((role: { name: string; }) => role.name).indexOf('admin');
+    const isManager = user?.roles.map((role: { name: string; }) => role.name)
+      .indexOf('manager');
+    const isAdmin = user?.roles.map((role: { name: string; }) => role.name)
+      .indexOf('admin');
 
     if (user !== null) {
       if (isManager !== -1) {
@@ -66,13 +74,14 @@ export default function MainNav({ user }: NavProps) {
         <div className="flex lg:flex-1">
           <Link className="-m-1.5 p-1.5" href={dashboardLink()}>
             <span className="sr-only">RexburgGuru</span>
-            <ApplicationLogo className="h-6 w-auto" />
+            <ApplicationLogo className="h-6 w-auto"/>
           </Link>
         </div>
         <div className="flex lg:hidden">
-          <button onClick={() => setMobileMenuOpen(true)} className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-slate-200">
+          <button onClick={() => setMobileMenuOpen(true)}
+                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-slate-200">
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
@@ -81,9 +90,10 @@ export default function MainNav({ user }: NavProps) {
             //   {item.name}
             // </Link>
             <Popover key={item.name} className="relative">
-              <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-slate-400 hover:text-white focus:outline-none">
+              <Popover.Button
+                className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-slate-400 hover:text-white focus:outline-none">
                 <span>{item.name}</span>
-                <ChevronDownIcon className="h-3 w-3" aria-hidden="true" />
+                <ChevronDownIcon className="h-3 w-3" aria-hidden="true"/>
               </Popover.Button>
 
               <Transition
@@ -96,17 +106,20 @@ export default function MainNav({ user }: NavProps) {
                 leaveTo="opacity-0 translate-y-1"
               >
                 <Popover.Panel className="absolute left-1/2 z-10 mt-4 flex w-screen max-w-max -translate-x-1/2 px-4">
-                  <div className="w-screen max-w-md flex-auto overflow-hidden rounded-2xl bg-slate-800 text-sm leading-6 shadow-lg ring-1 ring-slate-900/5">
+                  <div
+                    className="w-screen max-w-md flex-auto overflow-hidden rounded-2xl bg-slate-800 text-sm leading-6 shadow-lg ring-1 ring-slate-900/5">
                     <div className="p-4">
                       {item.links.map((link, index) => (
                         <div key={index} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-slate-700">
-                          <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-slate-600 group-hover:bg-slate-500">
-                            <link.icon className="h-6 w-6 text-slate-300 group-hover:text-slate-200" aria-hidden="true" />
+                          <div
+                            className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-slate-600 group-hover:bg-slate-500">
+                            <link.icon className="h-6 w-6 text-slate-300 group-hover:text-slate-200"
+                                       aria-hidden="true"/>
                           </div>
                           <div>
                             <Link href={link.href} className="font-semibold text-slate-100">
                               {link.name}
-                              <span className="absolute inset-0" />
+                              <span className="absolute inset-0"/>
                             </Link>
                             <p className="mt-1 text-slate-400">{link.description}</p>
                           </div>
@@ -123,10 +136,11 @@ export default function MainNav({ user }: NavProps) {
           {user ? (
             <Menu as="div" className="relative inline-block text-left">
               <div>
-                <Menu.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-slate-400 hover:text-white">
+                <Menu.Button
+                  className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-slate-400 hover:text-white">
                   <span className="sr-only">Open menu</span>
                   {user.name}
-                  <ChevronDownIcon className="-mr-0.5 h-3 w-3" />
+                  <ChevronDownIcon className="-mr-0.5 h-3 w-3"/>
                 </Menu.Button>
               </div>
 
@@ -139,7 +153,8 @@ export default function MainNav({ user }: NavProps) {
                 leaveFrom="opacity-100 translate-y-0"
                 leaveTo="opacity-0 translate-y-1"
               >
-                <Menu.Items className="absolute right-0 z-10 mt-4 w-48 origin-top-right rounded-2xl bg-slate-800 shadow-lg ring-1 ring-slate-900/5 focus:outline-none">
+                <Menu.Items
+                  className="absolute right-0 z-10 mt-4 w-48 origin-top-right rounded-2xl bg-slate-800 shadow-lg ring-1 ring-slate-900/5 focus:outline-none">
                   <div className="p-4 font-semibold">
                     <Menu.Item>
                       {({ active }) => (
@@ -175,7 +190,8 @@ export default function MainNav({ user }: NavProps) {
             </Menu>
           ) : (
             <>
-              <Link href={route('register')} className="rounded-md bg-[#B3D9E6] px-3.5 py-2.5 text-sm font-semibold text-slate-800 shadow-sm hover:bg-[#DBEDF3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
+              <Link href={route('register')}
+                    className="rounded-md bg-[#B3D9E6] px-3.5 py-2.5 text-sm font-semibold text-slate-800 shadow-sm hover:bg-[#DBEDF3] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
                 Register
               </Link>
               <Link href={route('login')} className="text-sm font-semibold leading-6 text-slate-400 hover:text-white">
@@ -186,12 +202,13 @@ export default function MainNav({ user }: NavProps) {
         </div>
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-slate-800 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="fixed inset-0 z-50"/>
+        <Dialog.Panel
+          className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-slate-800 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link className="-m-1.5 p-1.5" href={user ? route('dashboard') : route('welcome')}>
               <span className="sr-only">RexburgGuru</span>
-              <ApplicationLogo className="h-6 w-auto" />
+              <ApplicationLogo className="h-6 w-auto"/>
             </Link>
             <button
               type="button"
@@ -199,7 +216,7 @@ export default function MainNav({ user }: NavProps) {
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
             </button>
           </div>
           <div className="mt-6 flow-root">
@@ -216,12 +233,38 @@ export default function MainNav({ user }: NavProps) {
                 ))}
               </div>
               <div className="py-6">
-                <Link
-                  href={route('login')}
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-slate-400 hover:bg-slate-700"
-                >
-                  Log in
-                </Link>
+                {user !== null ? (
+                  <>
+                    <Link
+                      href={route('profile.edit')}
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-slate-400 hover:bg-slate-700"
+                    >
+                      Profile
+                    </Link>
+                    <Link
+                      href={route('logout')}
+                      method="post"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-slate-400 hover:bg-slate-700"
+                    >
+                      Log out
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <Link
+                      href={route('login')}
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-slate-400 hover:bg-slate-700"
+                    >
+                      Log in
+                    </Link>
+                    <Link
+                      href={route('register')}
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-slate-400 hover:bg-slate-700"
+                    >
+                      Register
+                    </Link>
+                  </>
+                )}
               </div>
             </div>
           </div>
