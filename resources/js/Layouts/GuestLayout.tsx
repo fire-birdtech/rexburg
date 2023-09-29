f2a18758ed3ff927f1da39a976431259b6d3c773
Jarom Rial <jarom.rial@proton.me>
2023-09-28 19:41:25 -0500

    fix the app logo display

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -7,7 +7,7 @@ export default function Guest ({ children }: PropsWithChildren): ReactElement {
     <div className="flex min-h-screen flex-col items-center justify-center bg-slate-900 bg-dots-lighter pt-6 selection:bg-sky-500 selection:text-white sm:justify-center sm:pt-0">
       <div>
         <Link href="/">
-          <ApplicationLogo className="h-10 w-auto fill-current text-slate-500 md:h-16" />
+          <ApplicationLogo className="h-10 w-auto md:h-16" />
         </Link>
       </div>
 
