d4738d10ff7e3189b8f24b17213f9b74f7195031
Jarom Rial <jarom.rial@proton.me>
2023-10-01 06:36:18 -0600

    add meta description to home page

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -9,7 +9,10 @@ export default function Welcome ({ auth }: PageProps): ReactElement {
 
   return (
     <>
-      <Head title="Welcome" />
+      <Head>
+        <title>Welcome</title>
+        <meta name="description" content="The go-to place for student housing at BYU-Idaho"/>
+      </Head>
 
       <div className="bg-slate-50 bg-dots-darker selection:bg-sky-500 selection:text-white dark:bg-slate-900 dark:bg-dots-lighter">
         <MainNav user={user} />
