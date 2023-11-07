e78512ff486e2d348573d3e3353ff52df254c466
Jarom Rial <jarom.rial@proton.me>
2023-11-07 13:58:33 -0700

    add doesntHaveRole method on User model to return opposite of hasRole method

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\HasProfilePhoto;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -76,6 +77,11 @@ class User extends Authenticatable implements MustVerifyEmail
         }
 
         return false;
+    }
+
+    public function doesntHaveRole($role): bool
+    {
+        return !$this->hasRole($role);
     }
 
     /**
