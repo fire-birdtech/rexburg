bf4df79b350ccb23ee8e46a902f99fd64e3a1b9d
Jarom Rial <jarom.rial@proton.me>
2023-11-07 13:13:24 -0700

    add businesses feature flag

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Pennant\Feature;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +23,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Feature::define('businesses', fn (User $user) => match (true) {
+            $user->hasRole('admin') => true,
+        });
     }
 }
