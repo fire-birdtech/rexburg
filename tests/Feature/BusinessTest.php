5332a73df62b474ba13c036dbb3b3dcaf2e254fc
Jarom Rial <jarom.rial@proton.me>
2023-11-07 06:18:59 -0700

    (pint) update php code styling

--- tests/Feature/BusinessTest.php
+++ tests/Feature/BusinessTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\Business;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BusinessTest extends TestCase
